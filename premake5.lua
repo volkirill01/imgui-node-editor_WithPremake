d2c691ba0935ec242187d21948dd171de2748020
Kirill <vologdinkirill48@gmail.com>
2024-02-09 11:47:21 +0600

    Add premake file

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+project "ImGui-node-editor"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.h",
+		"**.cpp",
+		"**.inl"
+	}
+
+	includedirs
+	{
+		"%{IncludeDir.ImGui}",
+		"external/DXSDK/include"
+	}
+
+	removefiles
+	{
+		"examples/**"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "on"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
